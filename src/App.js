b670357534f748db54bc17b238b05029ae81cf79
spitford <boyleb@hotmail.com>
2020-06-11 21:50:50 -0400

    Boilerplate React with express and bootstrap

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello World!</h1>
+      <button class="btn btn-primary">Button</button>
+    </div>
+  );
+}
+
+export default App;
