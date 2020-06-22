34d24e2741c697954645f7e98ca877b986a887ca
Owen-Ng <s.nigelowennlc@gmail.com>
2020-06-22 16:01:07 -0400

    Map and Dashboard completed

--- src/react-components/Maps/Map.item/Mapapi.js
+++ src/react-components/Maps/Map.item/Mapapi.js
@@ -1,72 +1,206 @@
 import React, { Component } from 'react';
-import OlMap from "ol/Map";
-import OlView from "ol/View";
-import OlLayerTile from "ol/layer/Tile";
-import OlSourceOSM from "ol/source/OSM";
-import {defaults as defaultInteractions, DragRotateAndZoom} from 'ol/interaction';
-class PublicMap extends Component {
+import { Map, Marker, Popup, TileLayer ,withLeaflet} from "react-leaflet";
+import { Icon } from "leaflet";
+import './Mapapi.css'
+import Search from "react-leaflet-search/lib/Search-v1";
+import * as parkdata from '../data/groceries.json'
+const someicon = new Icon ({iconUrl: "/cart.svg", iconSize:15}); 
+const active = new Icon({iconUrl:"/basket", iconSize: 20})
+export default class PublicMap extends Component {
   constructor(props) {
-    super(props);
-
-    this.state = { center: [0, 0], zoom: 1 };
-
-    this.olmap = new OlMap({
-        interactions: defaultInteractions().extend([
-            new DragRotateAndZoom()
-          ]),
-        dragging:true,
-      target: null,
-      DragRotateAndZoom:true,
-      layers: [
-        new OlLayerTile({
-          source: new OlSourceOSM()
-        })
-      ],
-      view: new OlView({
-        center: this.state.center,
-        zoom: this.state.zoom
-      })
-    });
+    super(props)
   }
-
-  updateMap() {
-    this.olmap.getView().setCenter(this.state.center);
-    this.olmap.getView().setZoom(this.state.zoom);
-  }
-
-  componentDidMount() {
-    this.olmap.setTarget("map");
-
-    // Listen to map changes
+  
+  state=null
+  //coor = this.props.city;
+  data(map){
     
-    this.olmap.on("moveend", () => {
-      let center = this.olmap.getView().getCenter();
-      let zoom = this.olmap.getView().getZoom();
-      this.setState({ center, zoom });
-    });
+    this.props.senddata(map.properties.NAME, map.properties.ADDRESS, map.properties.OPEN, map.properties.NOTES)
   }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    let center = this.olmap.getView().getCenter();
-    let zoom = this.olmap.getView().getZoom();
-    if (center === nextState.center && zoom === nextState.zoom) return false;
-    return true;
+  selected(park){
+    return(
+    <Marker key = {park.properties.PARK_ID} position={[
+      park.geometry.coordinates[1], 
+      park.geometry.coordinates[0]
+     
+    ]}
+    icon= {active}
+    />
+    )
   }
+ 
+  render(){
+    const SearchComponent = withLeaflet(Search);
+      return(
+        <div>
+        <Map center={[this.props.city[0], this.props.city[1]]} zoom={12}>
+               <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
+          />
+          <SearchComponent className="search"
+          customProvider={this.provider}
+          position="topright"
+          inputPlaceholder="Enter Address here"
+          search={[33.100745405144245, 46.48315429687501]}
+          showMarker={false}
+          zoom={5}
+          showPopup={false}
+          popUp={this.customPopup}
+          closeResultsOnClick={true}
+          openSearchOnLoad={true}
+          searchBounds={[
+            [33.100745405144245, 46.48315429687501],
+            [44.55916341529184, 24.510498046875]
+          ]}
+         
+         
+        />
+         
+        {parkdata.features.map(park =>(
+          <Marker key = {park.properties.PARK_ID} position={[
+            park.geometry.coordinates[1], 
+            park.geometry.coordinates[0]
+           
+          ]}
+          onClick={()=>{this.setState(park); this.data(park);this.selected(park);
 
-  userAction() {
-    this.setState({ center: [546000, 6868000], zoom: 5 });
+            
+          }}
+          icon = {someicon}/>
+         
+        )
+        )
+      }
+      {/* {this.state!='null'  && (
+        <Popup
+        
+        position={[
+          this.state.geometry.coordinates[1],
+          this.state.geometry.coordinates[0]
+        ]}
+        onClose={()=>{this.setState(null)}}
+        >
+          <div>
+            <h2>{this.state.properties.NAME}</h2>
+      <p>{this.state.properties.ADDRESS}</p>
+      <p>{this.state.properties.OPEN}</p>
+      <p>{this.state.properties.NOTES}</p>
+          </div>
+        </Popup>
+      )}
+       */}
+       {this.state && (
+       <Marker key = {this.state.properties.PARK_ID} position={[
+            this.state.geometry.coordinates[1], 
+            this.state.geometry.coordinates[0]
+           
+          ]}
+          icon = {active}/>)}
+        
+      </Map>
+{/* 
+     <button onClick={()=>console.log(this.props.name)}>♥</button>
+   <button onClick={()=>console.log(this.state)}>♥</button> */}
+   </div>
+  
+  )  
   }
+ }
 
-  render() {
-    this.updateMap(); // Update map on render?
-    return (
-      <div id="map" style={{ width: "100%", height: "100%" }}>
-      </div>
-    );
-  }
-}
 
-export default PublicMap;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import OlMap from "ol/Map";
+// import OlView from "ol/View";
+// import OlLayerTile from "ol/layer/Tile";
+// import OlSourceOSM from "ol/source/OSM";
+// import {defaults as defaultInteractions, DragRotateAndZoom} from 'ol/interaction';
+// class PublicMap extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = { center: [0, 0], zoom: 1 };
+
+//     this.olmap = new OlMap({
+//         interactions: defaultInteractions().extend([
+//             new DragRotateAndZoom()
+//           ]),
+//         dragging:true,
+//       target: null,
+//       DragRotateAndZoom:true,
+//       layers: [
+//         new OlLayerTile({
+//           source: new OlSourceOSM()
+//         })
+//       ],
+//       view: new OlView({
+//         center: this.state.center,
+//         zoom: this.state.zoom
+//       })
+//     });
+//   }
+
+//   updateMap() {
+//     this.olmap.getView().setCenter(this.state.center);
+//     this.olmap.getView().setZoom(this.state.zoom);
+//   }
+
+//   componentDidMount() {
+//     this.olmap.setTarget("map");
+
+//     // Listen to map changes
+    
+//     this.olmap.on("moveend", () => {
+//       let center = this.olmap.getView().getCenter();
+//       let zoom = this.olmap.getView().getZoom();
+//       this.setState({ center, zoom });
+//     });
+//   }
+
+//   shouldComponentUpdate(nextProps, nextState) {
+//     let center = this.olmap.getView().getCenter();
+//     let zoom = this.olmap.getView().getZoom();
+//     if (center === nextState.center && zoom === nextState.zoom) return false;
+//     return true;
+//   }
+
+//   userAction() {
+//     this.setState({ center: [546000, 6868000], zoom: 5 });
+//   }
+
+//   render() {
+//     this.updateMap(); // Update map on render?
+//     return (
+//       <div id="map" style={{ width: "100%", height: "100%" }}>
+//       </div>
+//     );
+//   }
+// }
+
+// export default PublicMap;
     
         
     
--- src/react-components/Maps/maps.component.js
+++ src/react-components/Maps/maps.component.js
@@ -14,37 +14,93 @@ export default class Maps extends Component {
           Hours:"",
           Wait_time:"",
         },
+       
         Lists:{ "List 1" : {Store:"Winners", Address:"MISSI RD", Hours:"8am-9pm", Wait_time:"20min"},
         "List 2": {Store:"Loblaws", Address:"Port RD", Hours:"8am-9pm", Wait_time:"20min"},
         "List 3": {Store:"Plavc", Address:"A ro RD", Hours:"8am-9pm", Wait_time:"20min"}},
-    }
+        City:{mississauga:[43.587684, -79.646186,],etobicoke:[43.618470, -79.514554],
+           toronto:[43.651717, -79.383545], scarborough:[43.774614, -79.259978],
+          york: [43.694012, -79.450578]}
+        ,citystate:"",
+        currentcity:"toronto",
+      }
     this.change = this.change.bind(this);
+    this.getdata= this.getdata.bind(this);
+    this.changecity = this.changecity.bind(this);
+    this.Keypress = this.Keypress.bind(this);
+  }
+  getdata(info, address, hour, wait){
+    const something ={Store:info,Address:address,Hours:hour,
+      Wait_time:wait,
+    } 
+    this.setState({currentstate:something})
   }
   change(item){
     const something= this.state.Lists[item];
 
     this.setState({currentstate:something});
   }
+  changecity(event){
+    console.log(event)
+    const target = event.target;
+    const value = target.value;
+    this.setState({citystate: value});
+  }
+  Keypress(event){
+    const key = event.Keycode || event.which;
+    
+    console.log(Object.keys(this.state.City).length)
+   
+          if (key === 13){
+            if(this.state.citystate in this.state.City){
+            this.setState(({citystate}) => ({
+              citystate: "",
+              currentcity: citystate,
+            
+            
+          }));
+          
+          console.log(this.state)
+          }
+          else{
+            alert("Does not exist")
+          
+        }
+   
+
+  }
+    
+
+  }
+  
   render() {
     return (
       <div class='container-xl' style={{ alignContent: 'center', justifyContent: 'center', }}>
         <div class="row">
           <div class="col-lg-9 border border-dark" style={{height:500}}>
-            <Map/>
-            <IconButton class ="b1"color='black' onClick={()=>this.change("List 1")}  >♥</IconButton>
+            <Map name = {this.state.currentcity}  city = {this.state.City[this.state.currentcity]} senddata= {this.getdata}/>
+            {/* <IconButton class ="b1"color='black' onClick={()=>this.change("List 1")}  >♥</IconButton>
             <IconButton class ="b2"color='black' onClick={()=>this.change("List 2")}>♥</IconButton>
-            <IconButton class ="b3"color='black' onClick={()=>this.change("List 3")}>♥</IconButton>
+            <IconButton class ="b3"color='black' onClick={()=>this.change("List 3")}>♥</IconButton> */}
           </div>
           <div class="p-0 m-0 col-3 border border-dark" >
             <div class="mapborderbottom" >
-              <input id="address" type="text" class="m-2 border-dark" placeholder="Enter address here" style={{width:"94%",textAlign: "center"}}/>
+              {/* <h2 class="header_map">Ceres</h2> */}
+              
+              <input name = "currentcity" 
+              value = {this.state.citystate} 
+              onChange={this.changecity} 
+              onKeyUp={this.Keypress} 
+              type="text" 
+              class="m-2 border-dark" placeholder="Enter City" 
+              style={{width:"94%",textAlign: "center"}}/>
             </div>
          
             <div class= "infor">
-            <p>Store Info:{this.state.currentstate.Store} </p>
-            <p>Address:{this.state.currentstate.Address} </p>
-            <p>Hours:{this.state.currentstate.Hours} </p>
-            <p>Wait time:{this.state.currentstate.Wait_time} </p>
+            <p>Store Info: <strong>{this.state.currentstate.Store}</strong> </p>
+            <p>Address: <strong>{this.state.currentstate.Address}</strong> </p>
+            <p>Hours: <strong>{this.state.currentstate.Hours}</strong> </p>
+            <p>Wait time: <strong>{this.state.currentstate.Wait_time}</strong> </p>
             </div>
             <div class="bottomtext">
               <span> Report how long your visit took</span>
