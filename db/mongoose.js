c0841419b09818ddf624335b2db1852e26e30dd6
Owen Ng <nigel.ngyanhing@mail.utoronto.ca>
2021-06-24 05:07:26 -0400

    Damn

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb+srv://demeter:OhyLZVWXe2WLE9NV@ceres.dois9.mongodb.net/CeresAPI?retryWrites=true&w=majority' || 'mongodb://localhost:27017/CeresAPI';
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/CeresAPI';
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 const connection = mongoose.connection;
